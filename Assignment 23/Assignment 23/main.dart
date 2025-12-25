58a783dceabdeaf32a4f9c7523c45f30555e6e14
Reduanul Islam Faroque <thamidulislamf@gmail.com>
2025-12-25 05:45:24 -0800

    Initialize Flutter app with task provider

---
+++ Assignment 23/Assignment 23/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/task_provider.dart';
+import './splash_screen.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task Manager',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SplashScreen(),
+    );
+  }
+}

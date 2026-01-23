f1ccf697ef88f286d4acbb6012140c771f1b723a
Reduanul Islam Faroque <thamidulislamf@gmail.com>
2026-01-23 02:20:29 -0800

    Integrate Firebase Crashlytics and auth state management
    Added Firebase Crashlytics error handling and user keys.

--- Assignment 25/main.dart
+++ Assignment 25/main.dart
@@ -1,15 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'sign_up_screen.dart';
+import 'dart:ui';
+import 'sign_in_screen.dart';
 import 'fcm_service.dart';
 import 'home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = (erroorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterError(erroorDetails);
+  };
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  FirebaseCrashlytics.instance.setCustomKey('userId', 12);
+  FirebaseCrashlytics.instance.setCustomKey('userRole', 'gest');
 
   await FcmService.initialize();
   print(await FcmService.getToken());
@@ -28,8 +46,17 @@ class LiveSprots extends StatefulWidget {
 class _LiveSprotsState extends State<LiveSprots> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SignUpScreen(),
+    return  MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, asyncSnapshot) {
+          if(asyncSnapshot.data != null){
+            return HomeScreen();
+          }else {
+            return signInScreen();
+          }
+        }
+      ),
     );
   }
 }
